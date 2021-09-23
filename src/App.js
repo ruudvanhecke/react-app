050853fdbce899e82fbf920fe139f6572df0c2ef
Vanhe <r0808423@student.thomasmore.be>
2021-09-23 11:57:18 +0200

    Start nieuwe app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
         <p>
           Ruud <code>Van </code> Hecke
         </p>
-        <a
-          className="App-link"
-          href="http://ruudvanhecke.be/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
   );
 }
 
